590acc4aebee00eb399600dc4d8b64917c142884
iammatthias <me@iammatthias.com>
2022-01-06 13:37:42 -0800

    update: json feed is now working

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-erp
+# what
+
+rustlang cli targetting wasm. runs on node and is deployed with npx/npm.
+
+all content is dynamically pulled from a json api
+
+## Building
+
+```
+cargo build --target=wasm32-unknown-emscripten --release
+```
+
+This moves files into:
+
+```
+./target/wasm32-unknown-emscripten/release/
+```
+
+Copy generated `card.js` and `card.wasm` into the root so the [bin](bin.js) can read it:
+
+```
+mv ./target/wasm32-unknown-emscripten/release/card.* .
+```
+
+Done!
+
+Once published the binary can be executed with npx:
+
+```
+npx iammatthias
+```
+
+Scripts are also discoverable in [package.json](package.json#L9)
+
+## License
+
+MIT
+
+### thanks
+
+Originally based on [https://github.com/rickycodes/card](https://github.com/rickycodes/card)
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,146 @@
+extern crate ansi_term;
+
+use ansi_term::Colour::Fixed;
+
+use reqwest;
+use serde::{Deserialize, Serialize};
 use std::env;
 
-mod card;
-mod print;
 mod colours;
 
-use card::Card;
-use print::print;
 use colours::colours;
 
+// general metadata
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Metadata {
+  pub name: String,
+  pub email: String,
+  pub handle: String,
+  pub role: String,
+  pub company: String,
+  pub web: String,
+  pub twitter: String,
+  pub git: String,
+}
+
+fn print_metadata(data: Metadata) {
+  let blue = 39;
+  let pink = 202;
+  let fixed_blue = Fixed(blue);
+  let fixed_pink = Fixed(pink);
+  let fixed_blue_bold = fixed_blue.bold();
+  let fixed_pink_bold = fixed_pink.bold();
+  let top = format!("{}{}", fixed_pink.paint("╭"), fixed_pink.paint(["─"; 64].join("")));
+  let space = format!("{}", [" "; 4].join(""));
+  let edge = fixed_pink.paint("│");
+  
+  println!("{}", top);
+  println!("{}{}", edge, " ");
+  println!("{}{}{} / {}", edge, space, fixed_blue_bold.paint(data.name), fixed_pink_bold.paint(data.handle));
+  println!("{}{}", edge, " ");
+  println!("{}{}{} @ {}", edge, space, data.role, data.company);
+  println!("{}{}", edge, " ");
+  println!("{}{}web: {}", edge, space, data.web);
+  println!("{}{}twitter: {}", edge, space, data.twitter);
+  println!("{}{}git: {}", edge, space, data.git);
+  println!("{}{}", edge, " ");
+}
+
+#[tokio::main]
+async fn metadata() {
+    let url = "https://iammatthias.com/api/feed/";
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        .send()
+        .await
+        .unwrap();
+    match response.status() {
+        reqwest::StatusCode::OK => {
+            match response.json::<Metadata>().await {
+                Ok(parsed) =>  print_metadata(parsed),
+                Err(_) => println!("Hm, the response didn't match the shape we expected.")
+            };
+        }
+        reqwest::StatusCode::UNAUTHORIZED => {
+            println!("Need to grab a new token");
+        }
+        other => {
+            panic!("Uh oh! Something unexpected happened: {:?}", other);
+        }
+    };
+}
+
+// recent content
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Feed {
+  pub version: String,
+  pub title: String,
+  pub home_page_url: String,
+  pub feed_url: String,
+  pub description: String,
+  pub author: Author,
+  pub items: Vec<Item>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Author {
+  pub name: String,
+  pub url: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Item {
+  pub id: String,
+  pub title: String,
+  pub date_modified: String,
+}
+
+fn print_feed(feed: Feed) {
+  print_content(feed.items.iter().take(4).collect())
+}
+
+fn print_content(items: Vec<&Item>) {
+  let pink = 202;
+  let fixed_pink = Fixed(pink);
+  let space = format!("{}", [" "; 2].join(""));
+  let edge = fixed_pink.paint("│");
+  for item in items {
+    println!("{}{}꩜ {}", edge, space, item.title);
+    println!("{}{}  {}", edge, space, item.id);
+    println!("{}{}", edge, " ")
+  }
+}
+
+#[tokio::main]
+async fn feed() {
+    let url = "https://iammatthias.com/api/feed/json/";
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        .send()
+        .await
+        .unwrap();
+    match response.status() {
+        reqwest::StatusCode::OK => {
+            match response.json::<Feed>().await {
+                Ok(parsed) =>  print_feed(parsed),
+                Err(_) => println!("Hm, the response didn't match the shape we expected.")
+            };
+        }
+        reqwest::StatusCode::UNAUTHORIZED => {
+            println!("Need to grab a new token");
+        }
+        other => {
+            panic!("Uh oh! Something unexpected happened: {:?}", other);
+        }
+    };
+}
 
 // output card
 fn card () {
-  let card = Card {
-    name: "Matthias Jordan",
-    title: "Growth",
-    handle: "iammatthias",
-    company: "Tordnado",
-    portfolio: "iammatthias.com",
-    resume: "iammatthias.com/resume",
-    twitter: "twitter.com/",
-    github: "github.com/",
-  };
-
-  print(card);
+  metadata();
+  feed();
 }
 
 fn main () {
